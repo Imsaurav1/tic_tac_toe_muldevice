68bd4b3edfc14620bc52d7dfd556969cbd40e434
Saurabh Kumar Jha <100080003+Imsaurav1@users.noreply.github.com>
2025-08-02 18:06:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public1")));
 
 let waitingPlayers = [];
 let games = {};
